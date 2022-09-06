f6b7e2ec5e3d37bd08e495a802610932e6d08b0f
BhavyasriC <112953659+BhavyasriC@users.noreply.github.com>
2022-09-06 22:33:01 +0530

    Add files via upload

---
+++ Program2.java
@@ -0,0 +1,14 @@
+package com.sep06;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+    int a =10,b=8;
+    a = a ^b;
+    b = a^b;
+    a = a^b;
+    System.out.println(a);
+    System.out.println(b);
+	}
+
+}
